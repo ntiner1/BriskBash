5357d3a0698eaf715821986255687bc101a50282
Nicholas Tiner <ntiner1@tamu.edu>
2018-08-09 22:52:26 -0500

    Setting up heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+// server
+
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+
+var port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('server started on [' + port + "]");
+});
